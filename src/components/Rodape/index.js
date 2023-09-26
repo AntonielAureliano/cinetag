2a1765d01b1e7814328f20d2fe62e078aa49249f
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-09-26 13:08:33 -0300

    Criando o rodapé.

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css";
+
+function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <h3>&copy; Desenvolvido por Antoniel Aureliano</h3>
+    </footer>
+  );
+}
+
+export default Rodape;
