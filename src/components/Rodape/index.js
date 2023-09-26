8e355689e924905ad6c266b22173d3855c314023
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-09-26 14:02:58 -0300

    Criando os componentes Titulo e Cards.

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -3,7 +3,7 @@ import styles from "./Rodape.module.css";
 function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <h3>&copy; Desenvolvido por Antoniel Aureliano</h3>
+      <h3>&copy; Desenvolvido por Antoniel Aureliano.</h3>
     </footer>
   );
 }
--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,18 @@
+import Banner from "../../components/Banner";
 import Cabecalho from "../../components/Cabecalho";
+import Card from "../../components/Card";
 import Rodape from "../../components/Rodape";
+import Titulo from "../../components/Titulo";
 
 function Inicio() {
   return (
     <>
       <Cabecalho />
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
+      <Card id="1" titulo="O Cortiço" capa="./homer-pelado.png" />
       <Rodape />
     </>
   );
